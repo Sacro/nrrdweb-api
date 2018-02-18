2cbe7ab108be2d6b5cfabc4ee88c564e6c29bf55
Ben Woodward <ben@benwoodward.me.uk>
2018-02-18 21:26:19 +0000

    Adding Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ApplicationModule } from './app.module';
 
 async function bootstrap() {
 	const app = await NestFactory.create(ApplicationModule);
+
+	app.setGlobalPrefix('api')
+
+	const options = new DocumentBuilder()
+		.setBasePath('api')
+		.setTitle('NRRD Api')
+		.setDescription('Network Rail Reference Data API')
+		.setVersion('1.0')
+		.build();
+	const document = SwaggerModule.createDocument(app, options);
+	SwaggerModule.setup('/docs', app, document);
+
 	await app.listen(3000);
 }
 bootstrap();
